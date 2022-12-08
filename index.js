872e75645de60c31051f9dc5f706902a77298014
Ashishk2509 <ashish.2509.k@gmail.com>
2022-12-08 13:17:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + "/uploads");
+  },
+  filename : (req, file, cb) => {
+    console.log(file);
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/upload', upload.single('file'), (req, res,) => {
+    res.send("Uploaded")
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`listening on port ${port}`)
+});
+
+
+
+
+
+//http://localhost:8080/filmmoz/upload_movie.php?title=" +title+ "&director_name=" +dirname+ "&actor_name=" +actor+ "&producer_name=" +producer+ "&story=" +story+ "&language=" +language+ "&category=" +category+ "&client_name=" +clientname+ "&cost=" +cost+ "&director_name=kiran patil
